2d711cdba4cc8be5f46217fa889df004fe369ef8
lucasliet <lucasouliveira@gmail.com>
2021-01-08 13:40:43 -0300

    visitando url por bot

---
+++ tests/features/specs/acessando_site.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+@acessando_url
+Funcionalidade: Acessar a primeira url.
+
+Cenário: Acessar a URL com sucesso.
+Quando acesso a url.
+Então eu verifico se estou na página correta.
--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -2,8 +2,8 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 
 Capybara.configure do |config|
-  # selenium  selenium_chrome selenium_chrome_headless (NOGPU)
-  config.default_driver = :selenium_chrome
+  # selenium (Firefox) selenium_chrome selenium_chrome_headless (NOGPU)
+  config.default_driver = :selenium
   config.app_host = 'https://automacaocombatista.herokuapp.com'
   config.default_max_wait_time = 5
 end
