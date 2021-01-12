712c3e3d600367db1eccfb42b89d9359662cd22d
lucasliet <lucasouliveira@gmail.com>
2021-01-12 20:06:26 -0300

    configurar variáveis de ambiente

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -11,9 +11,12 @@ World(Capybara::DSL)
 World(Capybara::RSpecMatchers)
 World(PageObjects)
 
+AMBIENTE = ENV['AMBIENTE']
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
+
 Capybara.configure do |config|
   # selenium (Firefox) selenium_chrome selenium_chrome_headless (NOGPU)
   config.default_driver = :selenium
-  config.app_host = 'https://automacaocombatista.herokuapp.com'
+  config.app_host = CONFIG['app_host']
   config.default_max_wait_time = 5
 end
