8c6482e269fc6a4a9c00f1cb3e9dd6f2d9d89421
lucasliet <lucasouliveira@gmail.com>
2021-01-12 20:54:38 -0300

    selenium config other browsers + headless

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -11,8 +11,29 @@ World(Capybara::DSL)
 World(Capybara::RSpecMatchers)
 World(PageObjects)
 
+BROWSER = ENV['BROWSER']
 AMBIENTE = ENV['AMBIENTE']
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
+
+
+Capybara.register_driver :selenium do |app|
+
+  if BROWSER.eql?('poltergeist')
+    options = { js_errors: false }
+    Capybara::Poltergeist::Driver.new(app, options)
+  elsif BROWSER.eql?('chrome_headless')
+    Capybara::Selenium::Driver.new(app, :browser => :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      'chromeOptions' => {'args' => ['--headless', 'disable-gpu']}
+    ))
+  elsif BROWSER.eql?('firefox_headless')
+    browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
+    Capybara::Selenium::Driver.new(app, :browser => :firefox, options: browser_options)
+  else
+    Capybara::Selenium::Driver.new(app, :browser => BROWSER.to_sym)
+  end
+
+end
 
 Capybara.configure do |config|
   # selenium (Firefox) selenium_chrome selenium_chrome_headless (NOGPU)
